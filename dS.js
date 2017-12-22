d6163cc0712a31812e1b02a289d5add7c16e0b7d
Adrian Llerena <adrianllerena@Adrians-MacBook-Pro.local>
2017-12-22 16:02:33 -0500

    finished adding to array

--- dS.js
+++ dS.js
@@ -15,3 +15,18 @@ var sarah = { name: "Sarah", age: 20 };
 var bill  = { name: "Bill", age: 19 };
 
 console.log(bob.name);
+
+var students = [
+  { name: "Bob", age: 17 },
+  { name: "Susy", age: 18 },
+  { name: "Ted", age: 18 },
+  { name: "Sarah", age: 20 },
+  { name: "Bill", age: 19 }
+];
+
+students.push({ name: "Steve", age: 52 });
+
+var beau = { name: "Beau", age: 21 };
+students.push(beau);
+
+console.log(students);
