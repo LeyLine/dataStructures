572a9a0621f43814796fd7f1b7a62ce355c19081
Adrian Llerena <adrianllerena@Adrians-MacBook-Pro.local>
2017-12-22 15:53:03 -0500

    first commit

---
+++ dS.js
@@ -0,0 +1,17 @@
+var students = [
+  "Bob",
+  "Susy",
+  "Ted",
+  "Sarah",
+  "Bill"
+];
+
+console.log(students[4]);
+
+var bob   = { name: "Bob", age: 17 };
+var susy  = { name: "Susy", age: 18 };
+var ted   = { name: "Ted", age: 18 };
+var sarah = { name: "Sarah", age: 20 };
+var bill  = { name: "Bill", age: 19 };
+
+console.log(bob.name);
